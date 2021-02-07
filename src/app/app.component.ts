4f058f22cc11e11dd9af6af6fd5c0c7218fed092
Mohamed Amine <mohamedaminebensafta@gmail.com>
2021-02-07 09:45:37 +0100

    séance 06/02/2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tuto';
+  nom:string="amine";
+  noms=["Amine","Amani","Yassmine","Boubaker"];
+  a=15;
+  b=14;
+  connected=true;
+  findT(){
+   // this.remplirTab();
+
+    //console.log(this.nom);
+    //let name=document.getElementById("v").textContent;
+    console.log(this.nom);
+    this.noms.push(this.nom);
+   // console.log(this.noms[i]);
+   // alert(this.nom);
+   // this.connected=false;
+  }
+  findById(){
+    let name=document.getElementById('t');
+    //name.innerHTML="Hey guys !";
+    console.log(name.innerText);
+  }
+  remplirTab(){
+    let i=this.noms.length;
+    let l=i+21;
+    while(i<l){
+      this.noms.push("etudiant "+i);
+      i++;
+    }
+  }
+  hey(id:number){
+    console.log(this.noms[id]);
+  }
+  delete(id:number){
+    let name=this.noms[id];
+    //this.noms.pop(id);
+  }
 }
