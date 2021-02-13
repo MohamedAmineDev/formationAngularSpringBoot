143f5cc6d4c480901629e72bc5a9d33afe86d3db
Mohamed Amine <mohamedaminebensafta@gmail.com>
2021-02-13 10:19:23 +0100

    séance 13/02/2021

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ import { crudService } from './services/crudService';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-apps;
-modeAf:string="true";
-// affichage :
- mode:number=0;
- verbe:string="afficher";
-  constructor(private service:crudService){
+  apps;
+  modeAf: string = "true";
+  // affichage :
+  mode: number = 0;
+  verbe: string = "afficher";
+  constructor(private service: crudService) {
 
   }
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
-    this.apps=this.service.getAppareil();
+    this.apps = this.service.getAppareil();
 
   }
   title = 'tuto';
@@ -64,48 +64,92 @@ modeAf:string="true";
     console.log("Update table !");
   }
 
-  modeAffichage(){
-if(this.mode==0){
-  this.mode=1;
-  this.verbe="masquer";
+  modeAffichage() {
+    if (this.mode == 0) {
+      this.mode = 1;
+      this.verbe = "masquer";
 
-}
-else{
-  this.mode=0;
-  this.verbe="afficher";
-}
+    }
+    else {
+      this.mode = 0;
+      this.verbe = "afficher";
+    }
   }
 
-  switchTo(){
-    this.modeAf="true";
+  switchTo() {
+    this.modeAf = "true";
   }
-  navigateTo(){
-    this.modeAf="false";
+  navigateTo() {
+    this.modeAf = "false";
   }
-  id:number;
-  name:string="";
-  statut:string="";
-  ajouter(){
-    let  appareil:Appareil=new Appareil(this.id,this.name,this.statut);
+  id: number;
+  name: string = "";
+  statut: string = "";
+  ajouter() {
+    let appareil: Appareil = new Appareil(this.id, this.name, this.statut);
     this.service.ajoutAppareil(appareil);
     alert("Ajout affecuté avec succès !");
     this.switchTo();
+    this.filtrer();
   }
 
 
   /**
    * controle de la couleur
    */
-  getColor():string{
-    if(this.name.length<5){
+  getColor(): string {
+    if (this.name.length < 5) {
       return "red";
     }
-    else{
+    else {
       return "green";
     }
 
   }
-  getvisible():string{
+  getvisible(): string {
     return "true";
   }
+  put: boolean = true;
+  filtrer(): void {
+    let tab = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    console.log(tab.filter(x => x > 3));
+  }
+  toSearch: boolean = false;
+  search() {
+    if (this.toSearch == false) {
+      this.toSearch = true;
+    }
+  }
+  trouver: string;
+  find() {
+    this.apps = this.service.search(this.trouver);
+  }
+
+  appareil: Appareil = new Appareil(0, "", "");
+  indice: number;
+  beforeUpdate(item: Appareil, i: number) {
+    this.appareil = item;
+    //this.updating(id);
+    this.indice = i;
+  }
+  updating() {
+    let test: boolean = this.service.updateApp(this.indice, this.appareil);
+    if (test) {
+      alert("Update effecuté !");
+    }
+    else {
+      alert("Update non effectué !");
+    }
+  }
+  test: boolean = false;
+
+  controler(): boolean {
+    if (this.id == 0 || this.name == '' || this.statut == '') {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
 }
--- src/app/services/crudService.ts
+++ src/app/services/crudService.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+
 import { Appareil } from "../model/appareil";
 
 @Injectable()
@@ -26,5 +27,29 @@ public getAppareil():any{
 }
 public  ajoutAppareil(appareil:Appareil):void{
   this.appareils.push(appareil);
+}
+
+public search(status:string):any{
+ if(status!="" ){
+  let listAppareil:any=this.appareils.filter(x=>x.status==status);
+  return listAppareil;
+ }
+ else{
+  return this.appareils;
+ }
+
+}
+
+public trouverApp(id:number):Appareil{
+  let appareil=this.appareils.find(x=>x.id=id);
+  return appareil;
+}
+public updateApp(id:number,appareil:Appareil):boolean{
+let test:boolean=false;
+this.appareils[id-1]=appareil;
+if(this.appareils[id-1]==appareil){
+  test=true;
+}
+return test;
 }
 }
