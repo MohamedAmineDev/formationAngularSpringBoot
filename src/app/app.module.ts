e6a305a414865dc8cd151fc53a9c451230683321
Mohamed Amine <mohamedaminebensafta@gmail.com>
2021-02-15 17:59:09 +0100

    séance 14/02/2021

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FormsModule } from "@angular/forms";
 import { crudService } from './services/crudService';
+import { Part2Component } from './part2/part2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Part2Component
   ],
   imports: [
     BrowserModule,
